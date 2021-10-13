6f93d8a53915a3dfb84cb8e91da161fc3305d37d
bilelmerhben <merhbenbilel91@gmail.com>
2021-10-13 13:24:14 +0100

    routing of components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { HomeComponent } from './home/home.component';
 import { InformationComponent } from './information/information.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes: Routes = [
 
-  /*{ path: '/information', component: InformationComponent }*/
-
+  { path: 'information', component: InformationComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'authentifier', component:AuthentificationComponent },
+  { path: '', component:HomeComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { InformationComponent } from './information/information.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    InformationComponent
+    InformationComponent,
+    InscriptionComponent,
+    AuthentificationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
