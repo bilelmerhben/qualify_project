1eb7524e6b13f4419719f9170666e5f46d03d4d3
bilelmerhben <merhbenbilel91@gmail.com>
2021-10-13 12:50:31 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InformationComponent } from './information/information.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  /*{ path: '/information', component: InformationComponent }*/
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
